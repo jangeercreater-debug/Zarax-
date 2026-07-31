4c19f3d874f34331dc81d3944a88ac5c507dafee
jangeercreater-debug <jangeercreater@gmail.com>
2026-07-31 18:09:22 +0530

    Update memory.module.ts

--- services/api/src/modules/memory/memory.module.ts
+++ services/api/src/modules/memory/memory.module.ts
@@ -1,5 +1,10 @@
 import { Module } from "@nestjs/common";
 import { MemoryController } from "./memory.controller";
+import { MemoryVectorService } from "./memory-vector.service";
 
-@Module({ controllers: [MemoryController] })
+@Module({
+  controllers: [MemoryController],
+  providers: [MemoryVectorService],
+  exports: [MemoryVectorService],
+})
 export class MemoryModule {}
