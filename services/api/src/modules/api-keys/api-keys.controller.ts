8eeb4976d12453a683c665e6ecaed7a099f65abc
jangeercreater-debug <jangeercreater@gmail.com>
2026-08-01 07:55:50 +0530

    Update api-keys.controller.ts

--- services/api/src/modules/api-keys/api-keys.controller.ts
+++ services/api/src/modules/api-keys/api-keys.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Delete, Body, Param, Inject, Query } from "@nestjs/common";
+import { Controller, Get, Post, Delete, Body, Param, Inject } from "@nestjs/common";
 import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { CurrentPrincipal, RequirePermission } from "@zarax/shared-auth";
 import { PRISMA_CLIENT, type PrismaClient } from "@zarax/database";
