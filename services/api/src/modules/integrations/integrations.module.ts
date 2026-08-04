24f5105add6af4b4fdf466ee692ff6286b132b38
jangeercreater-debug <jangeercreater@gmail.com>
2026-08-04 09:17:13 +0530

    Update integrations.module.ts

--- services/api/src/modules/integrations/integrations.module.ts
+++ services/api/src/modules/integrations/integrations.module.ts
@@ -3,9 +3,11 @@ import { IntegrationsController } from "./integrations.controller";
 import { TelegramModule } from "./telegram/telegram.module";
 import { SlackModule } from "./slack/slack.module";
 import { StripeModule } from "./stripe/stripe.module";
+import { WhatsAppModule } from "./whatsapp/whatsapp.module";
+import { DiscordModule } from "./discord/discord.module";
 
 @Module({
-  imports: [TelegramModule, SlackModule, StripeModule],
+  imports: [TelegramModule, SlackModule, StripeModule, WhatsAppModule, DiscordModule],
   controllers: [IntegrationsController],
 })
 export class IntegrationsModule {}
