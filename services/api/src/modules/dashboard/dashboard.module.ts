1ac55f166f325cd9e574e6bd8390247fe559a420
jangeercreater-debug <jangeercreater@gmail.com>
2026-08-04 10:01:28 +0530

    Update dashboard.module.ts

--- services/api/src/modules/dashboard/dashboard.module.ts
+++ services/api/src/modules/dashboard/dashboard.module.ts
@@ -1,5 +1,9 @@
 import { Module } from "@nestjs/common";
+import { RedisCacheModule } from "@zarax/redis-client";
 import { DashboardController } from "./dashboard.controller";
 
-@Module({ controllers: [DashboardController] })
+@Module({
+  imports: [RedisCacheModule.forRoot({ redisUrl: process.env.REDIS_URL ?? "" })],
+  controllers: [DashboardController],
+})
 export class DashboardModule {}
