476729da5e65cb2a5cb9a8f0a2c6908992dcd088
jangeercreater-debug <jangeercreater@gmail.com>
2026-08-04 08:35:29 +0530

    Update integrations.module.ts

--- services/api/src/modules/integrations/integrations.module.ts
+++ services/api/src/modules/integrations/integrations.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { IntegrationsController } from "./integrations.controller";
 import { TelegramModule } from "./telegram/telegram.module";
+import { SlackModule } from "./slack/slack.module";
+import { StripeModule } from "./stripe/stripe.module";
 
 @Module({
-  imports: [TelegramModule],
+  imports: [TelegramModule, SlackModule, StripeModule],
   controllers: [IntegrationsController],
 })
 export class IntegrationsModule {}
