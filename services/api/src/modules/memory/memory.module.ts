5741a9c91f5923f93166203f33270d23c5c23a26
jangeercreater-debug <jangeercreater@gmail.com>
2026-08-10 06:38:48 +0530

    Update memory.module.ts

--- services/api/src/modules/memory/memory.module.ts
+++ services/api/src/modules/memory/memory.module.ts
@@ -1,10 +1,20 @@
 import { Module } from "@nestjs/common";
+import { INTERNAL_SERVICE_TOKEN } from "@zarax/shared-auth";
 import { MemoryController } from "./memory.controller";
+import { InternalMemoryController } from "./internal-memory.controller";
+import { MemoryService } from "./memory.service";
 import { MemoryVectorService } from "./memory-vector.service";
 
 @Module({
-  controllers: [MemoryController],
-  providers: [MemoryVectorService],
-  exports: [MemoryVectorService],
+  controllers: [MemoryController, InternalMemoryController],
+  providers: [
+    MemoryService,
+    MemoryVectorService,
+    {
+      provide: INTERNAL_SERVICE_TOKEN,
+      useFactory: () => process.env.INTERNAL_SERVICE_TOKEN ?? "",
+    },
+  ],
+  exports: [MemoryService, MemoryVectorService],
 })
 export class MemoryModule {}
